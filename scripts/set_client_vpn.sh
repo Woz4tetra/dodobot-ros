0f91ae4cfcc0a346866df03494a85b05e34387b5
Woz4tetra <woz4tetra@gmail.com>
2020-09-13 10:49:20 -0400

    adding vpn set client script

--- scripts/set_client_vpn.sh
+++ scripts/set_client_vpn.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
-LOCAL_IP=`ifconfig wlan0 | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'`
-HOST_MACHINE=192.168.0.14
+LOCAL_IP=`ifconfig tun0 | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'`
+# LOCAL_IP=10.8.0.1
+HOST_MACHINE=10.8.0.6
 
 if [ -z ${LOCAL_IP} ]; then
     echo "Network not configured. Not setting ROS_IP or URI"
