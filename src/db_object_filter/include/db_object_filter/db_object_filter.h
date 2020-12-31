ad9bc9d1a5b1d5c9a831712d6bb7717138b6b915
Woz4tetra <woz4tetra@gmail.com>
2020-12-31 13:12:48 -0500

    object filter class receives object detections and assigns them the correct label

--- src/db_object_filter/include/db_object_filter/db_object_filter.h
+++ src/db_object_filter/include/db_object_filter/db_object_filter.h
@@ -8,6 +8,16 @@
 #include "ros/ros.h"
 #include "ros/console.h"
 
+#include <pdf/conditionalpdf.h>
+#include <pdf/gaussian.h>
+// #include "nonlinearMeasurementPdf.h"
+#include <wrappers/rng/rng.h>
+#include <filter/bootstrapfilter.h>
+#include <model/systemmodel.h>
+#include <model/measurementmodel.h>
+
+#include <vision_msgs/Detection2DArray.h>
+
 
 using namespace std;
 
@@ -17,6 +27,20 @@ private:
 
 public:
     DodobotObjectFilter(ros::NodeHandle* nodehandle);
+
+    // Launch parameters
+    string _class_label_param;
+    vector<string> _class_labels;
+
+    // Class labels
+    string to_label(int index);
+
+    // Subscribers
+    ros::Subscriber _detection_sub;
+
+    // Callbacks
+    void detections_callback(vision_msgs::Detection2DArray msg);
+
     int run();
 };
 
--- src/db_object_filter/src/db_object_filter/db_object_filter.cpp
+++ src/db_object_filter/src/db_object_filter/db_object_filter.cpp
@@ -4,9 +4,34 @@
 DodobotObjectFilter::DodobotObjectFilter(ros::NodeHandle* nodehandle) :
     nh(*nodehandle)
 {
+    ros::param::param<string>("~class_label_param", _class_label_param, "class_labels");
+    string key;
+    while (!ros::param::search(_class_label_param, key)) {
+        if (!ros::ok()) {
+            ROS_INFO_STREAM("db_object_filter interrupted while looking for " << _class_label_param);
+            return;
+        }
+        ros::Duration(0.05).sleep();
+    }
+
+    ROS_INFO_STREAM("db_object_filter found the class list parameter: " << key);
+    nh.getParam(key, _class_labels);
+
+    _detection_sub = nh.subscribe<vision_msgs::Detection2DArray>("detections", 25, &DodobotObjectFilter::detections_callback, this);
     ROS_INFO("db_object_filter init done");
 }
 
+string DodobotObjectFilter::to_label(int index) {
+    return _class_labels[index];
+}
+
+void DodobotObjectFilter::detections_callback(vision_msgs::Detection2DArray msg)
+{
+    for (size_t index = 0; index < msg.detections.size(); index++) {
+        vision_msgs::ObjectHypothesisWithPose obj = msg.detections[index].results[0];
+        ROS_INFO_STREAM("Found object: " << to_label(obj.id));
+    }
+}
 
 int DodobotObjectFilter::run()
 {
