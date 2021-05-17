f0acc8a3526ee4ea8e25aa41c5742d484adf8826
Woz4tetra <woz4tetra@gmail.com>
2021-05-17 02:07:01 -0400

    changing roscore script to run with ROS built from source

--- systemd/roscore.sh
+++ systemd/roscore.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-source /opt/ros/noetic/setup.sh
+source /home/ben/noetic_ws/install_isolated/setup.bash
 source /home/ben/.local/dodobot/dodobot-ros/bin/env.sh
 roscore & while ! echo exit | nc localhost 11311 > /dev/null; do sleep 1; done
