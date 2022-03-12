26c957745edac14c4f13a917ef44f820522636bb
Woz4tetra <woz4tetra@gmail.com>
2022-03-12 17:59:03 -0500

    changing launch and adding tutorial node

--- systemd/roslaunch.sh
+++ systemd/roslaunch.sh
@@ -13,7 +13,7 @@ tmux send -t $SESSION "sudo chmod 666 /dev/ttyTHS1" ENTER
 tmux send -t $SESSION "source /home/${USER}/packages_ros_ws/devel/setup.bash" ENTER
 tmux send -t $SESSION "source /home/${USER}/noetic_ws/install_isolated/setup.bash" ENTER
 tmux send -t $SESSION "source /home/${USER}/ros_ws/devel/setup.bash" ENTER
-tmux send -t $SESSION ". /usr/local/bin/env.sh 0 roslaunch" ENTER
+tmux send -t $SESSION ". /usr/local/bin/env.sh 0 roslaunch wlan1" ENTER
 
 tmux send -t $SESSION "export ROS_HOME=/home/${USER}/.ros" ENTER
 tmux send -t $SESSION "export DISPLAY=:0" ENTER
