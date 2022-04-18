da50bd7c54a4960eca552958928c589fc973b15f
Evan-FRC <egwiz01@gmail.com>
2022-04-18 15:40:09 -0400

    adding startup script

--- systemd/roslaunch.sh
+++ systemd/roslaunch.sh
@@ -10,10 +10,11 @@ if [ $? != 0 ]; then
 fi
 
 tmux send -t $SESSION "sudo chmod 666 /dev/ttyTHS1" ENTER
+tmux send -t $SESSION "source /home/${USER}/dodobot-ros/scripts/startup.sh" ENTER
 tmux send -t $SESSION "source /home/${USER}/packages_ros_ws/devel/setup.bash" ENTER
 tmux send -t $SESSION "source /home/${USER}/noetic_ws/install_isolated/setup.bash" ENTER
 tmux send -t $SESSION "source /home/${USER}/ros_ws/devel/setup.bash" ENTER
-tmux send -t $SESSION ". /usr/local/bin/env.sh 0 roslaunch" ENTER
+tmux send -t $SESSION ". /usr/local/bin/env.sh 0 roslaunch wlan1" ENTER
 
 tmux send -t $SESSION "export ROS_HOME=/home/${USER}/.ros" ENTER
 tmux send -t $SESSION "export DISPLAY=:0" ENTER
